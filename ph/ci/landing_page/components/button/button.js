5be94c8bb780f674a5e0425014a99a8641949594
Sabrina Luo <luohaojing@gmail.com>
2017-12-01 14:23:48 +0800

    add spinner into button

--- ph/ci/landing_page/components/button/button.js
+++ ph/ci/landing_page/components/button/button.js
@@ -1,10 +1,13 @@
 app.directive("abButton", function() {
   return {
+    replace: true,
     transclude: true,
     scope: {
       options: "=?"
     },
     templateUrl: "button/button.html",
-    link: function(scope, elem, attr) {}
+    link: function(scope, elem, attr) {
+      scope.options = scope.options || {};
+    }
   };
 });
