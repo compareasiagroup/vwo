b8479436d7e321bbd3031ad4e8f9624637d20ab8
Sabrina Luo <luohaojing@gmail.com>
2018-01-17 15:52:11 +0800

    add dropdown

---
+++ dk/ci/landing_page/components/dropdown/dropdown.js
@@ -0,0 +1,17 @@
+app.directive("abDropdown", [function() {
+  return {
+    replace: true,
+    scope: {
+      options: "=?",
+      model: "="
+    },
+    templateUrl: "dropdown/dropdown.html",
+    link: function(scope) {
+      scope.$watch('model', function(val) {
+        if (val) {
+          scope.options.showError = false;
+        }
+      });
+    }
+  };
+}]);
